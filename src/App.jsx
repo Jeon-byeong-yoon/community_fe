734ca445a030377c63fa6d7ebb73a0cef2514d57
Jeon Byeong Yoon <iamjack0416@naver.com>
2026-01-28 15:53:22 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <h1 className="text-4xl font-bold text-pink-400">
+        안녕하세요 
+      </h1>
+    </div>
+  );
+}
+
+
